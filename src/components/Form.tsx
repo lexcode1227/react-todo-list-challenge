70bb005475a9bd68409113eb89e62f913669054f
Henry Agustin <henryagustin297@gmail.com>
2024-12-23 14:53:57 -0600

    feat: add some components toorganize better the layout

---
+++ src/components/Form.tsx
@@ -0,0 +1,108 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import "./Form.css";
+import { todoSchema } from "../validations/dataSchema";
+import { FormData, mappedPriority } from "../types";
+
+const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(todoSchema),
+    defaultValues: {
+      completed: false,
+    },
+  });
+
+  const priorityOptions = Object.entries(mappedPriority).map(([key, value]) => (
+    <option key={key} value={key}>
+      {value}
+    </option>
+  ));
+
+  const onSubmit = (data: FormData) => {
+    console.log("Form data:", data);
+    const existingTasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+    existingTasks.push(data);
+    localStorage.setItem("tasks", JSON.stringify(existingTasks));
+  };
+
+  return (
+    <form className="form" onSubmit={handleSubmit(onSubmit)}>
+      <div className="formGroup">
+        <label className="label" htmlFor="taskName">
+          Task Name
+        </label>
+        <input
+          className="input"
+          id="taskName"
+          {...register("taskName", {
+            pattern: {
+              value: /^[A-Za-z\s]+$/,
+              message: "Task name must contain only alphabetic characters",
+            },
+          })}
+          required
+        />
+        {errors.taskName && <span>{errors.taskName.message}</span>}
+      </div>
+      <div className="formGroup">
+        <label className="label" htmlFor="priority">
+          Priority
+        </label>
+        <select
+          className="select"
+          id="priority"
+          {...register("priority")}
+          required
+        >
+          {priorityOptions}
+        </select>
+        {errors.priority && <span>{errors.priority.message}</span>}
+      </div>
+      <div className="formGroup">
+        <label className="label" htmlFor="storyPoints">
+          Story Points
+        </label>
+        <input
+          className="input"
+          id="storyPoints"
+          type="number"
+          {...register("storyPoints", { valueAsNumber: true })}
+          required
+        />
+        {errors.storyPoints && <span>{errors.storyPoints.message}</span>}
+      </div>
+      <div className="formGroup">
+        <label className="label" htmlFor="assignedTo">
+          Assigned To
+        </label>
+        <input
+          className="input"
+          id="assignedTo"
+          {...register("assignedTo")}
+          required
+        />
+        {errors.assignedTo && <span>{errors.assignedTo.message}</span>}
+      </div>
+      <div className="formGroup">
+        <label className="label" htmlFor="dueDate">
+          Due Date
+        </label>
+        <input
+          className="input"
+          id="dueDate"
+          type="date"
+          {...register("dueDate", { valueAsDate: true })}
+          required
+        />
+        {errors.dueDate && <span>{errors.dueDate.message}</span>}
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default Form;
---
+++ src/components/TaskList.tsx
@@ -0,0 +1,87 @@
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { SearchFormData, Task } from "../types";
+import "./TaskList.css";
+import { searchTodoSchema } from "../validations/dataSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const TaskList = () => {
+  const { register } = useForm<SearchFormData>({
+    resolver: zodResolver(searchTodoSchema),
+  });
+
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [searchValue, setSearchValue] = useState<string>("");
+
+  useEffect(() => {
+    const existingTasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+    const tasksWithIds = existingTasks.map((task: Task) =>
+      task.id ? task : { ...task, id: `${Date.now()}-${Math.random()}` }
+    );
+    setTasks(tasksWithIds);
+  }, []);
+
+  const filteredTasks = tasks.filter((task) =>
+    task.taskName.toLowerCase().includes(searchValue)
+  );
+
+  const handleDeleteTask = (id: string | undefined) => {
+    const updatedTasks = tasks.filter((task) => task.id !== id);
+    setTasks(updatedTasks);
+    localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+  };
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value.toLowerCase());
+  };
+
+  const handleCheckboxChange = (id: string | undefined) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === id ? { ...task, completed: !task.completed } : task
+    );
+    setTasks(updatedTasks);
+    localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+  };
+
+  return (
+    <section className="taskList">
+      <h2>Todo List</h2>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <div className="formGroup">
+          <label className="label" htmlFor="search">
+            Search
+          </label>
+          <input
+            className="input"
+            id="search"
+            {...register("search")}
+            placeholder="Search task by name..."
+            onChange={handleSearchChange}
+          />
+        </div>
+      </form>
+
+      {filteredTasks.length > 0 ? (
+        <ul className="taskListContainer">
+          {filteredTasks.map((task) => (
+            <li className="taskCard" key={task.id}>
+              <div className="taskCard-content">
+                <input
+                  type="checkbox"
+                  checked={task.completed}
+                  onChange={() => handleCheckboxChange(task.id)}
+                />
+                <p className="taskCard-title">{task.taskName}</p>
+              </div>
+              <button onClick={() => handleDeleteTask(task.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No se encontraron tareas con ese nombre</p>
+      )}
+    </section>
+  );
+};
+
+export default TaskList;
